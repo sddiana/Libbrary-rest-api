c450d77b7be2ff1cf68d04be72ccb431c6d5d33b
sddiana <mosethvatit@gmail.com>
2025-12-12 23:50:15 +0700

    Создание модели автора

--- main/models.py
+++ main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    """
+    Модель для представления автора книги.
+    """
+    #Обязательные поля
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    
+    # Биография (необязательное поле)
+    biography = models.TextField(blank=True, null=True)
+    
+    # Дата рождения (необязательное)
+    date_of_birth = models.DateField(blank=True, null=True)
+    
+    # Дата смерти (необязательное, для умерших авторов)
+    date_of_death = models.DateField(blank=True, null=True)
+
+    class Meta:
+        #Метаданные модели
+        ordering = ['last_name', 'first_name']
+
+    def __str__(self):
+        #Строковое представление объекта (для админки и shell)
+        return f'{self.last_name} {self.first_name}'
